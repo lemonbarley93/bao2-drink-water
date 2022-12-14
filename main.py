59771e9cf193cb4d836e0ff529b4caaac25d2b0d
lemonbarley93 <120554060+lemonbarley93@users.noreply.github.com>
2022-12-14 16:10:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import telebot
 import schedule
 from config import BOT_TOKEN, API_KEY,IDS_ALLOWED
 
-bot = telegram.Bot(token='5756035785:AAG7pD29YEfZuAojWEfEIkaMt3c-WG_y64k')
+bot = telegram.Bot(token='5982996987:AAFEh-PNYRCMCYfaC4dud5wXOta3Bq9xNOQ')
 
 def send_message():
 	chat_ids = [322888294, 484011551] # list of chat IDs
@@ -13,7 +13,7 @@ def send_message():
 	for chat_id in chat_ids:
 	    bot.send_message(chat_id=chat_id, text=message)
 
-schedule.every(3600).seconds.do(send_message)
+schedule.every(20).seconds.do(send_message)
 
 while True:
 	schedule.run_pending()
